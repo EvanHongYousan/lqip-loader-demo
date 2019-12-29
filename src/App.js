d3574b3bef2c2ef9905995f00f98e05ed4c2aefe
EvanHongYousan <1370204201@qq.com>
2019-12-29 14:26:27 +0800

    demo 完成

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import LqImg from './LqImg'
 
 function App() {
   return (
     <div className="App">
-      <img src={require('./images/1.jpg')}></img>
-      <img src={require('./images/2.jpg')}></img>
-      <img src={require('./images/3.jpg')}></img>
-      <img src={require('./images/4.jpg')}></img>
+      {/* <img src={require('./images/1.jpg').preSrc} alt="img"></img>
+      <img src={require('./images/2.jpg').preSrc} srcSet={require('./images/2.jpg').src} alt="img"></img>
+      <img src={require('./images/3.jpg').preSrc} alt="img"></img>
+      <img src={require('./images/4.jpg').preSrc} alt="img"></img> */}
+      <LqImg img={require('./images/1.jpg')} />
+      <LqImg img={require('./images/2.jpg')} />
+      <LqImg img={require('./images/3.jpg')} />
+      <LqImg img={require('./images/4.jpg')} />
     </div>
   );
 }
